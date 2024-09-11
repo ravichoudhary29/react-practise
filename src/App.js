86efa033ed1c2c639cd8fb4a9af8e7ddc66c26ec
Ravi Kumar <mailmeonravikumar@gmail.com>
2024-09-11 09:10:41 +0530

    1. Clean unnecessary files 2. CRUD operation: Add Create and Delete operation. 3. Add operation => lifting state up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+let newId = 3;
+const initialVal = [
+  { id: 1, name: "grapes" },
+  { id: 2, name: "apple" },
+];
 
 function App() {
+  const [data, setData] = useState(initialVal);
+
+  const handleDelete = (id) => {
+    setData((prev) => {
+      return prev.filter((t) => t.id !== id);
+    });
+  };
+
+  const handleAdd = (value) => {
+    setData((prev) => [
+      ...prev,
+      {
+        id: newId++,
+        name: value,
+      },
+    ]);
+  };
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewFruit onAdd={handleAdd} />
+      <ul>
+        {data.map((el) => {
+          return (
+            <li key={el.id}>
+              {el.name}
+              <button key={el.id} onClick={() => handleDelete(el.id)}>
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
+
+const NewFruit = ({ onAdd }) => {
+  const [fruit, setFruit] = useState("");
+  return (
+    <div>
+      <input value={fruit} onChange={(e) => setFruit(e.target.value)} />
+      <button onClick={() => onAdd(fruit)}>Add</button>
+    </div>
+  );
+};
 
 export default App;
